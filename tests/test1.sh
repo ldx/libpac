b8885502d05e7a1e58020a7ee8c6847b5eba370b
Vilmos Nebehaj <v.nebehaj@gmail.com>
2014-04-06 19:18:51 +0200

    Refactor test script.

--- tests/test1.sh
+++ tests/test1.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-test_proxy() {
-    _url="$1"
-    _host="$2"
-    _expected_proxy="$3"
-    _js="$(cat 1.js)"
-    _res=$(./test_pac "$_js" $_url $_host)
-    if [ "$_res" != "$_expected_proxy" ]; then
-        echo "FAILED:" $_res "!=" $_expected_proxy
-        exit 1
-    else
-        echo "OK:" $_url $_host "->" $_res
-    fi
-}
+RETVAL=0
+
+JS="$(cat 1.js)"
+
+. ./test_helper
 
 test_proxy http://abcdomain.com abcdomain.com "Found proxy DIRECT"
 test_proxy ftp://mydomain.com/x/ mydomain.com "Found proxy DIRECT"
@@ -23,4 +15,4 @@ test_proxy http://192.168.1.2/x/ 192.168.1.2 "Found proxy DIRECT"
 test_proxy http://127.0.0.5/x/ 127.0.0.5 "Found proxy DIRECT"
 test_proxy http://google.com/x google.com "Found proxy PROXY 4.5.6.7:8080; PROXY 7.8.9.10:8080"
 
-exit 0
+exit $RETVAL
