90447864068f6740a3db53645c262915a23a2e83
Vilmos Nebehaj <v.nebehaj@gmail.com>
2014-05-08 23:42:14 +0200

    Add unit test for pac_init().

---
+++ tests/test_unit1.c
@@ -0,0 +1,38 @@
+#include "greatest.h"
+
+#include "pac.h"
+
+SUITE(suite);
+
+TEST pac_init_valid_js(void)
+{
+    char *js = "function FindProxyForURL(u, h) { return \"DIRECT\"; }";
+
+    ASSERT(pac_init(js, NULL, NULL) == 0);
+
+    PASS();
+}
+
+TEST pac_init_invalid_js(void)
+{
+    char *js = "function FindProxyForURL(u, h) { return \"DIRECT\"; } foo;";
+
+    ASSERT(pac_init(js, NULL, NULL) != 0);
+
+    PASS();
+}
+
+GREATEST_SUITE(suite)
+{
+    RUN_TEST(pac_init_valid_js);
+    RUN_TEST(pac_init_invalid_js);
+}
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char **argv)
+{
+    GREATEST_MAIN_BEGIN(); /* command-line arguments, initialization. */
+    RUN_SUITE(suite);
+    GREATEST_MAIN_END(); /* display results */
+}
